16fab9b88986a78bee4de85d1b18b3bbd29de49d
ayoub aci <ayoubaci2002@gmail.com>
2024-07-18 00:10:10 +0200

    trying to make the header content responsive in smaller screens using javascript and css media queris to achieve that

---
+++ main.js
@@ -0,0 +1,22 @@
+/* script to handel header vevnts */
+/* this code to redesign the header when the media query matches*/
+function updateHeaderContent(){
+  /* creating a loop statement to iterate through the class names array*/
+  const iconsOrder = {0:"fa-linkedin", 1:"fa-discord", 2:"fa-github"};
+  for (let i = 0 ; i < 3; i++ ) {
+    
+    let updatedElement = document.getElementsByClassName("contact-links")[i];
+    /* checking if the current screen size matches the media query screen size specified in the condition*/
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+      updatedElement.innerHTML = `<i class="fa-brands ${iconsOrder[i]}"></i>`;
+      console.log(i);
+    } else {
+      updatedElement.innerHTML = `<i class="fa-brands ${iconsOrder[i]}></i> LinkedIn`;
+    }
+  }
+
+}
+/* inload check */
+updateHeaderContent();
+/*adding an event litener in case the media query matches the window screen size*/
+window.addEventListener("resize", updateHeaderContent);
