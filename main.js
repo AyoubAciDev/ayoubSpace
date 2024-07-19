31c5db090e51fe74d34f011b7e5196701bfb3cda
ayoub aci <ayoubaci2002@gmail.com>
2024-07-19 23:57:45 +0100

    did some changes to the main.js file concerning navbar logic

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 /* script to handel header vevnts */
 /* this code to redesign the header when the media query matches*/
 function updateHeaderContent(){
-  /* creating a loop statement to iterate through the class names array*/
-  const iconsOrder = {0:"fa-linkedin", 1:"fa-discord", 2:"fa-github"};
-  for (let i = 0 ; i < 3; i++ ) {
-    
+  /* creating a loop statement to iterate through the class names array*/ 
     let updatedElement = document.getElementsByClassName("contact-links")[i];
     /* checking if the current screen size matches the media query screen size specified in the condition*/
     if (window.matchMedia("(max-width: 1024px)").matches) {
@@ -13,8 +10,6 @@ function updateHeaderContent(){
     } else {
       updatedElement.innerHTML = `<i class="fa-brands ${iconsOrder[i]}></i> LinkedIn`;
     }
-  }
-
 }
 /* inload check */
 updateHeaderContent();
