96b113d0499da7d613faf224e076be277709a0b3
ayoub aci <ayoubaci2002@gmail.com>
2024-07-20 15:30:24 +0100

    still working

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 /* so using the window object we can konw the current screen size and using it's builtin function matchMedia we can compare the current screen size with the media screen size if so then I want u to update the innerhtml content of i tag to suit the design requerments*/
 function updateHeaderContent(){ 
   let i = 0;
-  let iconClasses = {0: "fa-linkedin", 1: "fa-discord", 2: "fa-fithub"};
+  let iconClasses = ["fa-linkedin", "fa-discord", "fa-github"];
   while (i < 3){
     let updatedElement = document.getElementsByClassName("contact-links")[i];
     /* checking if the current screen size matches the media query screen size specified in the condition*/
