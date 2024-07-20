a6ac1f6de2f0728cd64781ee1eb21dd615fc61b0
ayoub aci <ayoubaci2002@gmail.com>
2024-07-20 14:37:14 +0100

    still working on the navbar responsiveness

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 /* script to handel header vevnts */
 /* this code to redesign the header when the media query matches*/
-function updateHeaderContent(){
-  /* creating a loop statement to iterate through the class names array*/ 
+/* so using the window object we can konw the current screen size and using it's builtin function matchMedia we can compare the current screen size with the media screen size if so then I want u to update the innerhtml content of i tag to suit the design requerments*/
+function updateHeaderContent(){ 
+  let i = 0;
+  let iconClasses = {0: "fa-linkedin", 1: "fa-discord", 2: "fa-fithub"};
+  while (i < 3){
     let updatedElement = document.getElementsByClassName("contact-links")[i];
     /* checking if the current screen size matches the media query screen size specified in the condition*/
     if (window.matchMedia("(max-width: 1024px)").matches) {
-      updatedElement.innerHTML = `<i class="fa-brands ${iconsOrder[i]}"></i>`;
+      updatedElement.innerHTML = `<i class="fa-brands ${iconClasses[i]} "></i>`;
       console.log(i);
     } else {
-      updatedElement.innerHTML = `<i class="fa-brands ${iconsOrder[i]}></i> LinkedIn`;
+      updatedElement.innerHTML = `<i class="fa-brands ${iconClasses[i]} "></i> ${iconClasses[i]} `;
     }
+
+  }
 }
 /* inload check */
 updateHeaderContent();
