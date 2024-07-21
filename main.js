fd72f304e47e5dddf93c8e00b046689232c369a7
ayoub aci <ayoubaci2002@gmail.com>
2024-07-21 13:07:42 +0100

    made some chnges

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 /* I will add an event listener where if the navbar-trigger is clicked */
 /* we will add new html elements*/
 let clickableElement = document.querySelector(".navbar-trigger");
+/* let's create a variable that will tell us if the div is clicked*/
+isClickedBefore = false;
 function isClicked(){
   /* let's get the element that will be clickable*/
   /* check if the div is clicked*/
-  clickableElement.innerHTML = `<span class = "rotate span1"></span> <span class = "rotate span2"></span>`;
+  if (isClickedBefore == true){
+    clickableElement.innerHTML = `<span></span> <span></span>`;
+    isClickedBefore = false;
+  }
+  else{ 
+    clickableElement.innerHTML = `<span class = "rotate span1"></span> <span class = "rotate span2"></span>`;
+    isClickedBefore = true;
+  }
+
 
 }
 
